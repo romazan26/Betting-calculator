8fad438bd74a7a6edb79dd83bbb91fb1a767fd1d
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-10-14 21:39:15 +0300

    Initial Commit

---
+++ Betting calculator/Betting_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Betting_calculatorApp.swift
+//  Betting calculator
+//
+//  Created by Роман on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Betting_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
